2a35417984c8ea6d1b06b757b84a16006c44bc3d
pmbadger <pm@steepshot.io>
2021-08-28 22:33:52 +0300

    refactor: Corrected the BSC references to Telos

--- pancake-frontend/src/components/SearchModal/ImportToken.tsx
+++ pancake-frontend/src/components/SearchModal/ImportToken.tsx
@@ -31,7 +31,7 @@ function ImportToken({ tokens, handleCurrencySelect }: ImportProps) {
       <Message variant="warning">
         <Text>
           {t(
-            'Anyone can create a BEP20 token on Telos with any name, including creating fake versions of existing tokens and tokens that claim to represent projects that do not have a token.',
+            'Anyone can create a ERC20 token on Telos with any name, including creating fake versions of existing tokens and tokens that claim to represent projects that do not have a token.',
           )}
           <br />
           <br />
--- pancake-frontend/src/views/RemoveLiquidity/index.tsx
+++ pancake-frontend/src/views/RemoveLiquidity/index.tsx
@@ -112,7 +112,7 @@ export default function RemoveLiquidity({
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'Demo Swap LPs',
+      name: 'Pancake LPs',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address,
