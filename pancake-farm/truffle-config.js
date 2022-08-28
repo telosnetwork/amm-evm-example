8a937a91aad29cd306b178a554b57a22fea07b5b
pmbadger <pm@steepshot.io>
2022-08-28 12:09:57 +0200

    Deploy farm related contracts

--- pancake-farm/truffle-config.js
+++ pancake-farm/truffle-config.js
@@ -4,6 +4,7 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 
 module.exports = {
+  contracts_build_directory: "./build/stagenet/contracts",
   networks: {
     telos: {
       network_id: 41,
@@ -15,6 +16,16 @@ module.exports = {
       confirmations: 1,
       gas: 670000000,
       gasPrice: 2100000000000
+    },
+    stagenet: {
+      network_id: 41,
+      provider: () =>
+        new HDWalletProvider(
+          process.env.PRIVATE_KEY,
+          "http://testrpc.us.telos.net:7000/evm"
+        ),
+      gas: 6700000,
+      gasPrice: 500000000000
     },
   },
   compilers: {
